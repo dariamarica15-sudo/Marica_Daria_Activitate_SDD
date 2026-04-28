ef151639de22d7e8aaa41c1ea641831a696cf2ab
dariamarica15-studo <daria.marica15@gmail.com>
2026-04-28 22:29:47 +0300

    creez vector nou cu primele n produse

--- recap_vectori/recap_vectori/Source.c
+++ recap_vectori/recap_vectori/Source.c
@@ -38,20 +38,58 @@ void afisareVector(struct Produs* vector, int nrElemente) {
 	}
 }
 
+// Functie pentru copierea primelor n elemente dintr-un vector de produse
+
+struct Produs* copiazaPrimeleNElemente(struct Produs* vector, int nrElemente, int nrElementeCopiate) {
+
+	if (nrElementeCopiate > nrElemente) {
+		nrElementeCopiate = nrElemente;
+	}
+
+	struct Produs* vectorNou = (struct Produs*)malloc(sizeof(struct Produs) * nrElementeCopiate);
+
+	// Copiem elementele din vectorul original in vectorul nou
+	for (int i = 0; i < nrElementeCopiate; i++) {
+
+		vectorNou[i].id = vector[i].id;
+
+		vectorNou[i].denumire = (char*)malloc(strlen(vector[i].denumire) + 1);
+		strcpy(vectorNou[i].denumire, vector[i].denumire);
+
+		vectorNou[i].pret = vector[i].pret;
+		vectorNou[i].categorie = vector[i].categorie;
+	}
+
+	return vectorNou;
+}
+
+
 // Functie pentru eliberarea memoriei alocate pentru un produs
 int main() {
 	int nrProduse = 3;
 
 	struct Produs* prod = (struct Produs*)malloc(sizeof(struct Produs) * nrProduse);
-
+	
 	prod[0] = initializare(1, "Laptop", 3500.00, 'B');
 	prod[1] = initializare(2, "Telefon", 1500.00, 'T');
 	prod[2] = initializare(3, "Tableta", 2000.00, 'S');
-
+	
 	afisareVector(prod, nrProduse);
 
+	// Copiem primele 2 elemente din vectorul de produse
+	struct Produs* primeleDoua = copiazaPrimeleNElemente(prod, nrProduse, 2);
+	printf("Primele 2 produse:\n");
+	afisareVector(primeleDoua, 2);
+	
+	
+	// Eliberam memoria alocata pentru denumiri in vectorul original
 	for (int i = 0; i < nrProduse; i++) {
 		free(prod[i].denumire);
+	}
+
+	// Eliberam memoria alocata pentru primele doua produse copiate
+	for (int i = 0; i < 2; i++) {
+		free(primeleDoua[i].denumire);
 	}
 
 	free(prod);
